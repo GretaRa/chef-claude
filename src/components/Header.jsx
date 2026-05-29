d5feec810705ffb86b8929cebf6c86d99f38b154
Greta Ra <ggretute88@gmail.com>
2026-05-29 16:17:20 +0200

    create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+	return (
+		<header id="header">
+			<div id="logo">
+				<img id="logo-img" src="src/assets/chef-claude-icon.png" />
+				<span id="logo-text">Chef Claude</span>
+			</div>
+		</header>
+	);
+}
